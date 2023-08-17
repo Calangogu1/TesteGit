c4e34fe0b0c6867dc905c171d6c4b100786dc76e
Gustavo <silveiragustavo153@gmail.com>
2023-08-17 16:47:59 -0300

    Meu segundo commit

---
+++ src/teste/Produto.java
@@ -0,0 +1,22 @@
+package teste;
+
+public class Produto {
+
+	private String nome;
+	private String cor;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	
+}
